1f941965be3d67bf0e8df0117fed5e8967ff2f47
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2022-12-12 10:56:56 +0100

    Frontend
    hover changes bg color

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+// interaction
 let interaction = document.querySelector('a:nth-of-type(12)')
 
 interaction.addEventListener('click', jumpHandler)
@@ -6,3 +7,14 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Frontend
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('mouseover', backgroundChangeHover)
+frontend.addEventListener('mouseout', backgroundChangeHover)
+
+function backgroundChangeHover() {
+  frontend.classList.toggle('background')
+}
+
